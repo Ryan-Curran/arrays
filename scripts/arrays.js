ee034f23bf488802cd9e332e245db3e964ffb64d
Ryan-Curran <curranra@dukes.jmu.edu>
2022-10-21 13:29:00 -0400

    Friday

--- scripts/arrays.js
+++ scripts/arrays.js
@@ -1,47 +1,84 @@
 console.log(data);
 // 1. instead of creating the cards manually, we should use array functions to convert the data into cards
 
-const courseToCard = ({prefix, number, title, url, desc, prereqs, credits}) => {
+const courseToCard = ({
+  prefix,
+  number,
+  title,
+  url,
+  desc,
+  prereqs,
+  credits,
+}) => {
+  const prereqLinks = prereqs
+    .map((prereq) => `<a href="#" class="card-link">${prereq}</a>`)
+    .join();
   const courseTemplate = `<div class="col">
             <div class="card" style="width: 18rem;">
-              <h3 class="card-header">${title}</h3>
+              <h3 class="card-header"><a href="${url}">${title}</a></h3>
               <div class="card-body">
                 <h5 class="card-title">${prefix} ${number}</h5>
                 <p class="card-text">${desc}</p>
-                <h6 class="card-text">Prerequisites: ${prereqs}</h6>
+                ${prereqLinks}
+                <div class="card-footer text-muted">
+                  ${credits}
+                </div>
               </div>
             </div>
           </div>`;
   return courseTemplate;
-}
-
+};
+const resultsContainer = document.querySelector("#filtered-results");
 const courseCards = data.items.map(courseToCard);
-courseCards.forEach((c) => document.write(c));
+let filteredCourseCards = courseCards;
+resultsContainer.innerHTML = filteredCourseCards.join("");
+// courseCards.forEach((c) => document.write(c));
+
 // console.log(courseCards);
 // document.write(courseCards.join(''))
 
 // 2. maybe we only show those that match the search query?
+//
 
-// Approach: When search button is clicked, log query text, filter data, update dom
-
-const filterCards = (markup, query) => {
+const filterCourseCard = (markup, query) => {
+  console.log(markup, query);
   return markup.toLowerCase().includes(query.toLowerCase());
-}
+};
 
-const searchButton = document.getElementById("search-btn")
+const searchButton = document.getElementById("search-btn");
 searchButton.addEventListener("click", (ev) => {
   console.log(ev);
   ev.preventDefault();
+  // ev.stopPropagation();
   console.log("query text");
   const searchField = document.querySelector('input[name="query-text"]');
   const queryText = searchField.value;
   console.log(queryText);
-  const filteredCourseCards = courseCards.filter((card) => filteredCourseCards(card, queryText));
-  filteredCourseCards.forEach((card) => {
-    document.write(card);
-  })
-})
-
-
+  const filteredCourseCards = courseCards.filter((card) =>
+    filterCourseCard(card, queryText)
+  );
+  console.log('filteredCourseCards', filteredCourseCards);
+  resultsContainer.innerHTML = filteredCourseCards.join("");
+});
 
 // 3. we update the result count and related summary info as we filter
+
+// my attempt
+// const numPrereqs = courseCards.filter((card) => {
+//   `<dt>Count</dt>
+// <dd><span></span> items</dd>
+// <dt>Cost</dt>
+// <dd><span>12</span> credit-hours + <span>6</span> credit-hours of prereqs</dd>`;
+// return numPrereqs;
+// });
+// const prereqsContainer = document.querySelector("#live-count");
+// const updatePrereqs = data.items.map(numPrereqs);
+// countCost.innerHTML = updatePrereqs.join("");
+
+function updateCount() {
+  const count = document.getElementById("result-count");
+  const countValue = filteredCourseCards.length;
+  count.innerText = `${countValue} items`;
+}
+
+updateCount();
